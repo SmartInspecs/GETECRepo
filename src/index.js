40961cb4781a79307ebd20fb18a03e5b50c0bb0d
PedroAfonsoBraga <pedroavfb@gmail.com>
2024-08-18 13:42:53 -0300

    add: speedinsights

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/globalStyle";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <SpeedInsights />
     <BrowserRouter>
       <GlobalStyle />
       <App />
